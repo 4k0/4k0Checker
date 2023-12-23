f2be069e76756b3cb74e648578b940942be0258c
angel a <88174864+4k0@users.noreply.github.com>
2023-12-23 11:00:36 -0700

    im stupid

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 # config.py
 
-WEBHOOK_URL = "https://discord.com/api/webhooks/1188046523188117555/zmTaRQ6DWOTcS1P2TeMDcDfVSIJSVftZP9HH6c47xqIy1yNxKZ7XrVSSQ4V6JTuksfmT"
+WEBHOOK_URL = "https://discord.com/api/webhooks/urhookhere"
